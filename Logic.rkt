88291e2b0339ad27a9690921e46ba0a0e01ca1e7
Kevin <skryfall@gmail.com>
2019-08-17 15:15:41 -0600

    Dijkstra
    Agregado el algoritmo de dijkstra para el grafo

--- Logic.rkt
+++ Logic.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname src) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname Logic) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 >(define grafo null)
 
 >(define (getGrafo)
@@ -61,7 +61,7 @@
                    (anadirAlGrafo_aux2 idAgregar (cdr ids) (cdr pesos) (cdr bi) nGrafo i rIds rPesos rBi rGrafo)))))))
 
 >(define (revisarAdyacentes nodo)
-   (cond ((< nodo 1)
+   (cond ((or (< nodo 1) (> nodo (largoGrafo)))
           '())
          (else
           (revisarAdyacentes_aux nodo 1 grafo))))
@@ -81,7 +81,7 @@
           (revisarAdyacentes_aux2 (cdr fila) adj (+ j 1)))))
 
 >(define (revisarPeso nodo1 nodo2)
-   (cond ((or (< nodo1 1) (< nodo2 1) (> nodo1 15) (> nodo2 15))
+   (cond ((or (< nodo1 1) (< nodo2 1) (> nodo1 (largoGrafo)) (> nodo2 (largoGrafo)))
           -1)
          ((= nodo1 nodo2)
           0)
@@ -100,11 +100,139 @@
          (else
           (revisarPeso_aux2 n1 n2 (+ j 1) (cdr fila)))))
 
+>(define (listaIds)
+   (cond ((null? grafo)
+          '())
+         (else
+          (listaIds_aux grafo 1))))
+
+>(define (listaIds_aux rGrafo i)
+   (cond ((null? rGrafo)
+          '())
+         (else
+          (append (list i) (listaIds_aux (cdr rGrafo) (+ i 1))))))
+
+>(define (largoGrafo)
+   (cond ((null? grafo)
+          0)
+         (else
+          (largoGrafo_aux grafo))))
+
+>(define (largoGrafo_aux rGrafo)
+   (cond ((null? rGrafo)
+          0)
+         (else
+          (+ 1 (largoGrafo_aux (cdr rGrafo))))))
+
+>(define (infoNodo nodo)
+   (cond ((or (< nodo 1) (> nodo (largoGrafo)))
+          '())
+         (else
+          (infoNodo_aux nodo (revisarAdyacentes 5) '()))))
+
+>(define (infoNodo_aux nodo adj nAdj)
+   (cond ((null? adj)
+          (infoNodo_aux2 nodo nAdj '() nAdj))
+         ((= nodo (car adj))
+          (infoNodo_aux nodo (cdr adj) nAdj))
+         (else
+          (infoNodo_aux nodo (cdr adj) (append nAdj (list (car adj)))))))
+
+>(define (infoNodo_aux2 nodo adj pesos rAdj)
+   (cond ((null? adj)
+          (append (list rAdj) (list pesos)))
+         (else
+          (infoNodo_aux2 nodo (cdr adj) (append pesos (list (revisarPeso nodo (car adj)))) rAdj))))
+
+>(define (iniciarDistancia distancia i largo inicio)
+   (cond ((> i largo)
+          distancia)
+         ((= i inicio)
+          (iniciarDistancia (append distancia (list 0)) (+ i 1) largo inicio))
+         (else
+          (iniciarDistancia (append distancia (list 999)) (+ i 1) largo inicio))))
+
+>(define (iniciarVisitados visitados i largo)
+   (cond ((> i largo)
+          visitados)
+         (else
+          (iniciarVisitados (append visitados (list 0)) (+ i 1) largo))))
+
+>(define (iniciarCamino camino i largo inicio)
+   (cond ((> i largo)
+          camino)
+         ((= i inicio)
+          (iniciarCamino (append camino (list -1)) (+ i 1) largo inicio))
+         (else
+          (iniciarCamino (append camino (list 0)) (+ i 1) largo inicio))))
+
+>(define (buscarElemento i posicion lista)
+   (cond ((null? lista)
+          -1)
+         ((= i posicion)
+          (car lista))
+         (else
+          (buscarElemento (+ i 1) posicion (cdr lista)))))
+
+>(define (modificarElemento nuevoElemento i posicion lista nuevaLista)
+   (cond ((null? lista)
+          nuevaLista)
+         ((= i posicion)
+          (modificarElemento nuevoElemento (+ i 1) posicion (cdr lista) (append nuevaLista (list nuevoElemento))))
+         (else
+          (modificarElemento nuevoElemento (+ i 1) posicion (cdr lista) (append nuevaLista (list (car lista)))))))
+
+>(define (modificarLista listas nuevaListas i numeroDeLista nuevoElemento posicion)
+   (cond ((null? listas)
+          nuevaListas)
+         ((= i numeroDeLista)
+          (modificarLista (cdr listas) (append nuevaListas (list (modificarElemento nuevoElemento 1 posicion (car listas) '()))) (+ 1 i) numeroDeLista nuevoElemento posicion))
+         (else
+          (modificarLista (cdr listas) (append nuevaListas (list (car listas))) (+ i 1) numeroDeLista nuevoElemento posicion))))
+
+>(define (dijkstra inicio fin)
+   (cond ((or (< inicio 1) (< fin 1) (> inicio (largoGrafo)) (> fin (largoGrafo)))
+          '())
+         (else
+          (dijkstra_aux inicio fin (largoGrafo) (append (list (iniciarDistancia '() 1 (largoGrafo) inicio)) (list (iniciarVisitados '() 1 (largoGrafo))) (list (iniciarCamino '() 1 (largoGrafo) inicio))) 1))))
+
+>(define (dijkstra_aux inicio fin numNodos listas i)
+   (cond ((= i numNodos)
+          (dijkstra_aux4 numNodos inicio fin (car listas) (caddr listas) '()))
+         (else
+          (dijkstra_aux inicio fin numNodos (dijkstra_aux2 numNodos listas -1 999 1) (+ i 1)))))
+
+>(define (dijkstra_aux2 numNodos listas nodoCercano distanciaCorta j)
+   (cond ((> j numNodos)
+          (dijkstra_aux3 numNodos (modificarLista listas '() 1 2 1 nodoCercano) nodoCercano distanciaCorta (revisarPeso nodoCercano 1) 1))
+         ((and (= 0 (buscarElemento 1 j (cadr listas))) (< (buscarElemento 1 j (car listas)) distanciaCorta))
+          (dijkstra_aux2 numNodos listas j (buscarElemento 1 j (car listas)) (+ j 1)))
+         (else
+          (dijkstra_aux2 numNodos listas nodoCercano distanciaCorta (+ j 1)))))
+
+>(define (dijkstra_aux3 numNodos listas nodoCercano distanciaCorta peso k)
+   (cond ((> k numNodos)
+          listas)
+         ((and (> peso 0) (< (+ distanciaCorta peso) (buscarElemento 1 k (car listas))))
+          (dijkstra_aux3 numNodos (append (list (modificarElemento (+ distanciaCorta peso) 1 k (car listas) '())) (list (cadr listas)) (list (modificarElemento nodoCercano 1 k (caddr listas) '()))) nodoCercano distanciaCorta (revisarPeso nodoCercano (+ k 1)) (+ k 1)))
+         (else
+          (dijkstra_aux3 numNodos listas nodoCercano distanciaCorta (revisarPeso nodoCercano (+ k 1)) (+ k 1)))))
+
+>(define (dijkstra_aux4 numNodos inicio fin distancias caminos caminoFinal)
+   (cond ((= fin -1)
+          caminoFinal)
+         (else
+          (dijkstra_aux4 numNodos inicio (buscarElemento 1 fin caminos) distancias caminos (append (list fin) caminoFinal)))))
+
 >(anadirAlGrafo 1 '(0) '(0) '(0))
 >(anadirAlGrafo 2 '(1) '(70) '(0))
 >(anadirAlGrafo 3 '(1 2) '(90 76) '(0 1))
->(anadirAlGrafo 4 '(3) '(12) '(0))
+>(anadirAlGrafo 4 '(3 2) '(12 65) '(0 1))
 >(anadirAlGrafo 5 '(1 2 4) '(10 23 34) '(1 1 1))
 >(getGrafo)
->(revisarAdyacentes 5)
->(revisarPeso 3 4)
+;>(revisarAdyacentes 5)
+;>(revisarPeso 3 4)
+;>(listaIds)
+;>(infoNodo 5)
+;>(largoGrafo)
+>(dijkstra 3 4)
